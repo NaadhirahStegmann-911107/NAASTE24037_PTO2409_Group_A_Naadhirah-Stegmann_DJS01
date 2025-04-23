586023756edc63731021a9677234006fee3eedb9
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-04-23 23:18:32 +0200

    Corrected Parameters
    Refactored parameter constants in order to have the correct units if measurements in the calculation.
    
    Made comments for converting km/h to m/s and having the initialDistance in m instead of km.

--- index.js
+++ index.js
@@ -5,16 +5,16 @@
  * 3. Make these calculations robust such that the calculation does not give an incorrect result, it throws an error to the user if something has gone wrong (parameter used with an incorrect unit of measurement, etc)
  */
 
-// Given Parameters
-const vel = 10000; // velocity (km/h)
-const acc = 3; // acceleration (m/s^2)
+// Given Parameters in correct units of measurement
+const initialVelocityMs = 10000 * 1000 / 3600; // velocity converted from (km/h) to (m/s)
+const acceleration = 3; // acceleration (m/s^2)
 const time = 3600; // seconds (1 hour)
-const d = 0; // distance (km)
-const fuel = 5000; // remaining fuel (kg)
-const fbr = 0.5; // fuel burn rate (kg/s)
+const initialDistance = 0; // distance must be in meters (m) and can be converted to (km)
+const initialFuel = 5000; // remaining fuel (kg)
+const fuelBurnRate = 0.5; // fuel burn rate (kg/s)
 
 
-const d2 = d + (vel*time) //calcultes new distance
+const d2 = d + (vel*time) //calcultes new distance 
 const rf = fbr*time //calculates remaining fuel
 const vel2 = calcNewVel(acc, vel, time) //calculates new velocity based on acceleration
 
